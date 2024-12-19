832a6f9ca5a4f9ff4de06d13bb1e2315d81d74ea
Perrorin <perrocyb@gmail.com>
2024-12-19 11:50:55 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const ouvrirmenu = () => {
+    const menu = document.querySelector(".header-menu");
+    menu.classList.toggle("active")
+    if(menu.classList.contains("active")){
+        document.querySelector("header .material-icons").innerHTML ="close"
+    }
+    else{
+        document.querySelector("header .material-icons").innerHTML ="menu"
+    }
+}
